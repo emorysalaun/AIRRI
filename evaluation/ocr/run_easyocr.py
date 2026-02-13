60ea56400899e7798034f2a8423479fc359f071e
Emory Salaun <emory_salaun@uri.edu>
2026-02-13 12:41:42 -0500

    Added relative pathing to run_easyocr

--- evaluation/ocr/run_easyocr.py
+++ evaluation/ocr/run_easyocr.py
@@ -1,29 +1,27 @@
-import os
+from pathlib import Path
 import easyocr
 import warnings
+
 warnings.filterwarnings("ignore")
 
 
-RENDERS_DIR = "data/renders"
+BASE_DIR = Path(__file__).resolve().parent.parent
+# Relative pathing
+RENDERS_DIR = BASE_DIR / "data" / "renders"
 
-# Initialize reader
 reader = easyocr.Reader(['en'], gpu=False)
 
-# Only get pictures here.
-for filename in sorted(os.listdir(RENDERS_DIR)):
-    if not filename.lower().endswith((".png", ".jpg", ".jpeg")):
+for image_path in sorted(RENDERS_DIR.iterdir()):
+    if image_path.suffix.lower() not in [".png", ".jpg", ".jpeg"]:
         continue
 
-    image_path = os.path.join(RENDERS_DIR, filename)
+    print("=" * 50)
+    print("FILE:", image_path.name)
+    print("=" * 50)
 
-    print("=" * 60)
-    print("FILE:", filename)
-    print("=" * 60)
-
-    result = reader.readtext(image_path)
-
+    result = reader.readtext(str(image_path))
     texts = [txt for _, txt, _ in result]
     ocr_joined = " ".join(texts)
 
     print(ocr_joined)
-    print("\n")
+    print()
