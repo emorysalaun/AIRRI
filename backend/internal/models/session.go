36c22f9e83af9d6682a21c85b8d1acb79b3b27d7
emoryss1 <emory_salaun@uri.edu>
2026-04-06 10:44:44 -0400

    Added models folder for backend

---
+++ backend/internal/models/session.go
@@ -0,0 +1,9 @@
+package models
+
+import "time"
+
+type Session struct {
+	ID        string     `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time  `json:"created_at"`
+	EndedAt   *time.Time `json:"ended_at,omitempty"`
+}
