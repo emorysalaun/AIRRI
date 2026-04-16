f4b77763be55a701dbb3634c62cde9e9eb6f8d20
emoryss1 <emory_salaun@uri.edu>
2026-04-16 13:23:46 -0400

    Small backend restructure

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,16 +5,21 @@ import (
 	"airri-backend/internal/handlers"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	db.Init()
 
 	e := echo.New()
+	e.Use(middleware.CORS())
 
-	// register routes
+	// health check route
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "AIRRI backend running")
+	})
+
 	e.POST("/api/v0/interactions", handlers.CreateInteractionEvent)
 
-	// start server
 	e.Logger.Fatal(e.Start(":8080"))
 }
