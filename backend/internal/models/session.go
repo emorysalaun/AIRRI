88c41495a47b99188d9e256c53c5b3285646a410
emoryss1 <emory_salaun@uri.edu>
2026-04-16 12:29:31 -0400

    Changed session model to not require user

--- backend/internal/models/session.go
+++ backend/internal/models/session.go
@@ -4,8 +4,8 @@ import "time"
 
 type Session struct {
 	ID        string     `gorm:"primaryKey" json:"id"`
-	UserID    string     `gorm:"column:user_id;not null"`
-	User      User       `gorm:"foreignKey:UserID;references:ID"`
+	UserID    *string    `gorm:"column:user_id;index" json:"user_id,omitempty"`
+	User      *User      `gorm:"foreignKey:UserID;references:ID" json:"user,omitempty"`
 	CreatedAt time.Time  `json:"created_at"`
 	EndedAt   *time.Time `json:"ended_at,omitempty"`
 }
