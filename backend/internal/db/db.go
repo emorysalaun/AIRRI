96a4f5e6138653de34bace5aa906e1cafc78db71
emoryss1 <emory_salaun@uri.edu>
2026-04-16 12:36:35 -0400

    Added interaction_event table & updated migration

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -9,14 +9,21 @@ import (
 )
 
 var DB *gorm.DB
-
+	
 func Init() {
 	db, err := gorm.Open(sqlite.Open("../data/airri.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("failed to connect database:", err)
 	}
 
-	err = db.AutoMigrate(&models.Session{})
+	err = db.AutoMigrate(
+		&models.User{},
+		&models.Session{},
+		&models.RenderConfig{},
+		&models.Render{},
+		&models.OCRResult{},
+		&models.InteractionEvent{},
+	)
 	if err != nil {
 		log.Fatal("failed to migrate database:", err)
 	}
